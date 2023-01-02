113842b047128310213855db8ad537ef3facb128
LAI <lukalai1@orange.fr>
2023-01-02 12:04:39 +0100

    Commit Lecon Lundi matin

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");
+
+Test C1 = new Test();
+C1.Booleen = true;
+Test C2 = C1;
+
+C1.Booleen = false;
+
+Console.WriteLine(C2.Booleen);
+public class Test
+{
+    public bool Booleen;
+}*/
+
+using System;
+namespace ConsoleApp{    
+    internal static class Program{
+        public static void Main(string[] args){
+            Console.WriteLine("Hello, World");
+        }
+    }
+}
